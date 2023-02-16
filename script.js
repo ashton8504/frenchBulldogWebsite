877f4b097ffeea4b65de6be1b754891310b22683
ashton8504 <ashtonlittle8504@gmail.com>
2023-02-16 12:58:40 -0500

    saving

---
+++ script.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  // additional JavaScript code here if needed
+});
